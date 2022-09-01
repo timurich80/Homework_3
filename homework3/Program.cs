a2dadca01db75312ada3acb675d8ac2f7aca668f
timurich80 <timurich80@gmail.com>
2022-09-01 19:16:04 +0300

    код работает

---
+++ homework3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 
+*/
+
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+int count = 1;
+int  cub = 0;
+
+
+for (count = 1; count <= n; count++)
+{
+    cub = b * b * b;
+    Console.Write(n + "-> " + cub + "  ");
+    b++;
+}
